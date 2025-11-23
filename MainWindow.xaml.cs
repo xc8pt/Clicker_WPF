e7f53013eb779283bd46928959d36dc21729e66d
xc8pt <brolike636@gmail.com>
2025-11-23 09:05:35 +0300

    Refactoring

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,31 @@
-﻿using System.Text;
+﻿using System.Diagnostics.PerformanceData;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Clicker_WPF
 {
     public partial class MainWindow : Window
     {
+        private int _counter = 0;
         public MainWindow()
         {
             InitializeComponent();
+            UpdateCounterText();
+        }
+        private void UpdateCounterText()
+        {
+            CounterText.Text = $"Счётчик: {_counter}";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _counter++;
+            UpdateCounterText();
+        }
+        private void Button_Reset(object sender, RoutedEventArgs e)
+        {
+            _counter = 0;
+            UpdateCounterText();
         }
     }
 }
